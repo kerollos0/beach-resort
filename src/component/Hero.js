786082ca05707df6780e2dbfa8143cb2134256c2
kerollos0 <kerozaher888@gmail.com>
2020-06-07 00:36:59 +0200

    k

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Banner from "../component/Banner";
+import { Link } from "react-router-dom";
 
-export default function Hero({ children, hero }) {
-  return <header className={hero}>{children}</header>;
+export default function Hero({ hero }) {
+  return (
+    <header className={hero}>
+      <Banner
+        title="luxurious rooms"
+        subtitle="deluxe rooms starting
+        at $299"
+      >
+        <Link to="/rooms" className="btn-primary">
+          Our Rooms
+        </Link>
+      </Banner>{" "}
+    </header>
+  );
 }
 Hero.defaultProps = {
   hero: "defaultHero",
